498723c94a1382d7a659acdf1f5a63c99f938f58
Mani8639 <159261260+Mani8639@users.noreply.github.com>
2025-03-03 15:21:11 +0530

    Update pickup_app.py

--- pickup_app.py
+++ pickup_app.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 
 # Load the trained CatBoost model
-model = pickle.load(open("pickup_app.py", "rb"))
+model = pickle.load(open("model_pickup.pkl", "rb"))
 
 st.title("ETA Prediction of Pickup")
 st.write("Enter details to predict the Estimated Time of Arrival (ETA) for pickup.")
